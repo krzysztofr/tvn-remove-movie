874cb8784ee17479f354763af0ce4cf1b28820b8
Krzysztof Rakowski <krzysztof@rakowski.pl>
2014-05-30 18:49:30 +0200

    tvn meteo added

--- tvn-remove-movie.user.js
+++ tvn-remove-movie.user.js
@@ -5,13 +5,13 @@
 // @author         Krzysztof Rakowski
 // @license        MIT
 // @homepage       http://github.com/krzysztofr/tvn-remove-movie
-// @version        1.0
+// @version        1.3
 // @match          http://tvn24.pl/*
 // @match          http://www.tvn24.pl/*
 // @match          http://tvnwarszawa.tvn24.pl/*
 // @match          http://tvn24bis.pl/*
 // @match          http://www.tvn24bis.pl/*
-// @require        https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js
+// @match          http://tvnmeteo.tvn24.pl/*
 // ==/UserScript==
 
 // tvn24.pl
@@ -21,6 +21,11 @@ if (typeof e[0] != 'undefined'){
 }
 
 e = document.getElementsByClassName('videoVideo');
+if (typeof e[0] != 'undefined'){
+	e[0].parentNode.removeChild(e[0]);
+}
+
+e = document.getElementsByClassName('fl');
 if (typeof e[0] != 'undefined'){
 	e[0].parentNode.removeChild(e[0]);
 }
